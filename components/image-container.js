88d5852dbd0ce6c94a6cc7f8719da3f7fa4ba717
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-20 15:29:48 +0000

    change pics

--- components/image-container.js
+++ components/image-container.js
@@ -1,14 +1,33 @@
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
 
 const DynamicComponentWithNoSSR = dynamic(
   () => import('../components/model'),
   { ssr: false }
 )
 
-const ImageContainer = () => (
-  <div className="col-12@md col-6">
-    <DynamicComponentWithNoSSR />
-  </div>
-)
+const ImageContainer = () => {
+  let [isModel, setModel] = useState(false)
+  let [image, setImage] = useState("/shirt.jpg")
+
+  let updateImage = (e) => {
+    setModel(false)
+    setImage(e.target.src)
+  }
+  
+  return(
+    <div className="col-12@md col-6 container__row u-pad-v-md u-pad-r-sm">
+      <div className="col-10 col-12@md u-pad-b-xs">
+        { isModel ? <DynamicComponentWithNoSSR /> : <img src={image}  /> }
+      </div>
+      <div className="col-2 col-12@md u-pad-h-xs@md-min container__row">
+        <img className="col-2@md u-pad-r-sm@md u-pad-b-sm@md-min" src="/shirt.jpg" onClick={(e) => updateImage(e)}/>
+        <img className="col-2@md u-pad-r-sm@md u-pad-b-sm@md-min" src="/shirt.jpg" onClick={(e) => updateImage(e)}/>
+        <img className="col-2@md u-pad-r-sm@md u-pad-b-sm@md-min" src="/shirt.jpg" onClick={(e) => updateImage(e)}/>
+        <img className="col-2@md u-pad-r-sm@md" src="/box.png" onClick={() => setModel(true)}/>
+      </div>
+    </div>
+    )
+}
 
 export default ImageContainer
--- components/model.js
+++ components/model.js
@@ -4,14 +4,15 @@ const ModelComp = () => {
   
   const modelStyle = {
     width: '100%',
-    height: '600px',
+    height: '500px',
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    outline: 0
   }
 
   const posterStyles = {
     width: '100%',
-    height: '600px',
+    height: '500px',
     backgroundImage: 'url("/box.png")',
     position: 'absolute', 
     top: "0", 
@@ -20,7 +21,7 @@ const ModelComp = () => {
   }
 
   return (
-    <div className="col-12 u-pad-v-sm u-pad-r-sm u-height-500">
+    <div className="col-12 u-height-500">
       <model-viewer style={modelStyle} poster="/box.png" src='/box.glb' auto-rotate camera-controls reveal="interaction" background-color="#455A64" ar id="lazy-load" ar-scale="auto">
         <div id="lazy-load-poster" style={posterStyles} slot="poster"></div>
         <div id="button-load" className="c-btn c-btn--model u-mar-v-sm u-mar-h-sm" slot="poster">Load 3D Model</div>
