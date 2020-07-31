d28a000446935124e9e5cc361e24ab69ef3cc01a
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-07-31 15:27:20 +0100

    change to a chair

--- components/image-container.js
+++ components/image-container.js
@@ -8,7 +8,7 @@ const DynamicComponentWithNoSSR = dynamic(
 
 const ImageContainer = () => {
   let [isModel, setModel] = useState(false)
-  let [image, setImage] = useState("/box-mr-meeks.png")
+  let [image, setImage] = useState("/HUG-ARMCHAIR_1.jpg")
 
   let updateImage = (e) => {
     setModel(false)
@@ -21,7 +21,8 @@ const ImageContainer = () => {
         { isModel ? <DynamicComponentWithNoSSR /> : <img className="col-12" src={image}  /> }
       </div>
       <div className="col-2 col-12@md u-pad-h-xs@md-min container__row">
-        <img className="col-2@md u-pad-r-sm@md u-pad-b-xs@md-min" src="/box-mr-meeks.png" onClick={(e) => updateImage(e)}/>
+        <img className="col-2@md u-pad-r-sm@md u-pad-b-xs@md-min" src="/HUG-ARMCHAIR_1.jpg" onClick={(e) => updateImage(e)}/>
+        <img className="col-2@md u-pad-r-sm@md u-pad-b-xs@md-min" src="/HUG-ARMCHAIR_2.jpg" onClick={(e) => updateImage(e)}/>
         <img className="col-2@md u-pad-r-sm@md" src="/box-3D-Message.png" onClick={() => setModel(true)}/>
       </div>
     </div>
--- components/model.js
+++ components/model.js
@@ -22,7 +22,7 @@ const ModelComp = () => {
 
   return (
     <div className="col-12 u-height-500">
-      <model-viewer style={modelStyle} poster="/box.png" src='/box.glb' auto-rotate camera-controls reveal="interaction" background-color="#455A64" ar id="lazy-load" ar-scale="auto">
+      <model-viewer style={modelStyle} poster="/box.png" src='/chair.glb' auto-rotate camera-controls reveal="interaction" background-color="#455A64" ar id="lazy-load" ar-scale="auto">
         <div id="lazy-load-poster" style={posterStyles} slot="poster"></div>
         <div id="button-load" className="c-btn c-btn--model u-mar-v-sm u-mar-h-sm" slot="poster">Load 3D Model</div>
       </model-viewer>
